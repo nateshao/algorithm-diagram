f20ba2287a03175b13c67358ea9fe4e5ecf2ec0f
nateshao <shaotongjie@gmail.com>
2020-12-03 22:16:21 +0800

    init

--- 1.二分查找/01_binary_search/src/com/nateshao/BinarySearch.java
+++ 1.二分查找/01_binary_search/src/com/nateshao/BinarySearch.java
@@ -1,10 +1,17 @@
 package com.nateshao;
-
 /**
  * 如果你不明白其优缺点，这些实现将毫无用处
  */
-public class Main {
-
+/**
+ * @date Created by 邵桐杰 on 2020/12/3 22:15
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description：
+ */
+public class BinarySearch {
     public static void main(String[] args) {
         int[] myList = {87, 21, 45, 93};
 
